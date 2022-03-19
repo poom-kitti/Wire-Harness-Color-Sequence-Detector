1a157b7342233568e7ea7974d04fc708e169f48d
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2022-03-19 13:30:18 +0700

    feat: Implement util function to filter for max contour

--- src/utils/contour_utils.py
+++ src/utils/contour_utils.py
@@ -29,3 +29,32 @@ def filter_out_small_contours(
     cv2.drawContours(thresh_img, contours_to_fill, -1, 0, -1)
 
     return thresh_img
+
+
+def filter_for_largest_contour(
+    thresh_img: np.ndarray, min_contour_area: float
+) -> np.ndarray:
+    """Filter for the largest contour in `thresh_img`. Any contour that is not the largest
+    contour will be filled as black.
+
+    The largest contour must be larger than specified `min_contour_area` otherwise
+    a simple black image will be returned.
+
+    Expects `thresh_img` to be a grayscale image with bg as black as fg as white.
+    """
+    # Make a black image
+    black_img = np.zeros(thresh_img.shape, np.uint8)
+
+    # Find the contours
+    contours, _ = cv2.findContours(
+        thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+
+    # Get the max contour
+    max_contour = max(contours, key=cv2.contourArea)
+
+    # Fill the black image with white at the max_contour if greater than min_contour_are
+    if cv2.contourArea(max_contour) >= min_contour_area:
+        cv2.drawContours(black_img, [max_contour], -1, 255, -1)
+
+    return black_img
