5475cc563d2d748249cbe284f18599489e718027
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2022-04-03 19:28:00 +0700

    feat: Implement final variables for input keys

---
+++ src/stages/keys.py
@@ -0,0 +1,7 @@
+"""This module contains the acceptable keys for each stage."""
+N_KEY = ord("n")
+Q_KEY = ord("q")
+Y_KEY = ord("y")
+ESC_KEY = 27
+
+ACCEPTABLE_KEYS_FOR_INITIALIZE_STAGE = [Y_KEY, N_KEY, Q_KEY, ESC_KEY]
